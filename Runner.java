e4ebb837ec1e053e0642d180551eeb85165f6206
Ellimyst <sumukhvaze@gmail.com>
2016-06-16 11:33:58 +0530

    create animals

--- Runner.java
+++ Runner.java
@@ -8,13 +8,22 @@ public class Runner {
 	public static void main(String[] args) {
 		
 		ArrayList<Tree> trees = new ArrayList<Tree>();
+		ArrayList<Animal> animals = new ArrayList<Animal>();
 		
 		for(int i = 0; i < 10; i++)
 		{
 			trees.add(new Tree(10,4));
 		}
 		
+		for(int i = 0; i < 5; i++)
+		{
+			animals.add(new Animal(9,2));
+		}
+		
+		
+		
 		System.out.println(trees);
+		System.out.println(animals);
 	}
 
 }
